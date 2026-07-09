d6adf38e074d0ae02c8dbee82f894369d4d28624
Castro <jamillymilly10@hotmail.com>
2026-07-09 15:55:12 -0300

    Initial commit

---
+++ src/main/java/br/com/anunciada/stewards_api/StewardsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.anunciada.stewards_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StewardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StewardsApiApplication.class, args);
+	}
+
+}
