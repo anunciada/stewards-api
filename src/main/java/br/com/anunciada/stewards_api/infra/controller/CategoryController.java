e6d1abb5c875912ba61cf91b7ac4ef77c2156fe6
anunciada <jamillymilly10@hotmail.com>
2026-07-10 14:26:47 -0300

    feat: impl general listing of categories

--- src/main/java/br/com/anunciada/stewards_api/infra/controller/CategoryController.java
+++ src/main/java/br/com/anunciada/stewards_api/infra/controller/CategoryController.java
@@ -2,19 +2,30 @@ package br.com.anunciada.stewards_api.infra.controller;
 
 import br.com.anunciada.stewards_api.application.usecase.category.CreateCategoryCommand;
 import br.com.anunciada.stewards_api.application.usecase.category.CreateCategoryUseCase;
+import br.com.anunciada.stewards_api.application.usecase.category.ListCategoriesUseCase;
+import br.com.anunciada.stewards_api.domain.model.Category;
 import br.com.anunciada.stewards_api.infra.controller.request.CreateCategoryRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
         private final CreateCategoryUseCase createCategoryUseCase;
+        private final ListCategoriesUseCase listCategoriesUseCase;
 
-        public CategoryController(CreateCategoryUseCase createCategoryUseCase) {
+        public CategoryController(CreateCategoryUseCase createCategoryUseCase,
+                                  ListCategoriesUseCase listCategoriesUseCase) {
             this.createCategoryUseCase = createCategoryUseCase;
+            this.listCategoriesUseCase = listCategoriesUseCase;
         }
 
         @PostMapping
@@ -26,6 +37,12 @@ public class CategoryController {
                             request.groupId()
                     )
             );
+        }
+
+        @GetMapping
+        @ResponseStatus(HttpStatus.OK)
+        public List<Category> listCategory() {
+            return listCategoriesUseCase.execute();
         }
 
 }
